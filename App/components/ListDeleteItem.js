efa0254f9964bb12c74b6d8c0c418978a40c25c4
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-07-30 17:33:22 +0530

    comiit

--- App/components/ListDeleteItem.js
+++ App/components/ListDeleteItem.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 import React from "react";
 
 export default function ListDeleteItem() {
   return (
-    <View>
-      <Text>ListDeleteItem</Text>
-    </View>
+      <TouchableWithoutFeedback>
+          
+    </TouchableWithoutFeedback>
   );
 }
 
