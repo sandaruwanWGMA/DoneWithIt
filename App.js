fbad4badbf5cdaa56d530e21fd013b9edb822889
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-08-01 11:29:15 +0530

    comiit

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import WelcomeScreen from "./App/screens/WelcomeScreen";
 import RenderDeleteAction from "./App/components/RenderDeleteAction";
 import Icon from "./App/components/Icon";
 import ListItem from "./App/components/ListItem";
-import Screen from "./App/components/Screen";
 import MessagesScreen from "./App/screens/MessagesScreen";
 import AppTextInput from "./App/components/AppTextInput";
 import AppText from "./App/components/AppText";
+import Screen from "./App/components/Screen";
 
 export default function App() {
   return (
-    <MessagesScreen></MessagesScreen>
+    <Screen>
+      <AppTextInput
+        placeholder="Username"
+        iconName="gmail"
+      ></AppTextInput>
+    </Screen>
   );
 }
--- App/components/AppTextInput.js
+++ App/components/AppTextInput.js
@@ -1,19 +1,47 @@
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import React from "react";
+import { MaterialCommunityIcons } from "expo-vector-icons";
 
 import Screen from "./Screen";
 import colors from "../config/colors";
 
-export default function AppTextInput({ placeholder }) {
+export default function AppTextInput({
+  iconName,
+  size = 20,
+  placeholder,
+  keyboardType,
+  onChangeText,
+}) {
   return (
-    <Screen>
-      <TextInput style={styles.textInput}>{placeholder}</TextInput>
-    </Screen>
+    <View style={styles.container}>
+      {iconName && (
+        <MaterialCommunityIcons
+          name={iconName}
+          size={size}
+          color={colors.medium}
+        ></MaterialCommunityIcons>
+      )}
+      <TextInput
+        style={[styles.textInput, { fontSize: size }]}
+        keyboardType={keyboardType}
+        onChangeText={onChangeText}
+      >
+        {placeholder}
+      </TextInput>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-    textInput: {
-      backgroundColor: "#f2f2f2"
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#f2f2f2",
+    padding: 10,
+  },
+  textInput: {
+    marginLeft: 10,
+    color: colors.medium,
   },
 });
